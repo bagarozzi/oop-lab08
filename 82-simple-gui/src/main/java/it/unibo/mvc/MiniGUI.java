714e31b496f82157ff51d12c5eada9c52120de69
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-14 15:49:51 +0100

    TextField now has the random value everytime button is pressed

--- 82-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 82-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -49,7 +49,9 @@ public class MiniGUI {
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                int randomValue = randomGenerator.nextInt();
+                System.out.println(randomValue);
+                resultField.setText(Integer.toString(randomValue));
             }
         });
     }
