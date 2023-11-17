2b093ea922444eb1172061be930d930922535225
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-17 09:45:40 +0100

    Implementation of point 2

---
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,80 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote {
+    /**
+     * Returns the rule with the given number.
+     *
+     * @param ruleNumber the number of the rule to return. The first rule has number one
+     * @return the rule with the given number
+     * @throws IllegalArgumentException if the given rule number is smaller than 1 or larger
+     * than the number of rules
+     */
+    public String getRule(int ruleNumber) {
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * The human whose name is written in this DeathNote will die.
+     * @param name the name of the human to kill
+     * @throws NullPointerException if the given name is null.
+     */
+    public void writeName(String name){
+        throw new NullPointerException();
+    }
+
+    /**
+     * If the cause of death is written within the next 40 milliseconds of writing the person's
+     * name, it will happen.
+     *
+     * @param cause the cause of the human's death
+     * @return true if the cause was written within 40 milliseconds, false otherwise
+     * @throws IllegalStateException if there is no name written in this DeathNote,
+     * or the cause is null
+     */
+    public boolean writeDeathCause(String cause){
+        throw new IllegalStateException();
+    }
+
+    /**
+     * After writing the cause of death, details of the death should be written in the next
+     * 6 seconds and 40 milliseconds.
+     *
+     * @param details the details of the human's death
+     * @return true if the details were written within 6 seconds and 40 milliseconds, false otherwise
+     * @throws IllegalStateException if there is no name written in this DeathNote,
+     * or the details are null
+     */
+    public boolean writeDetails(String details){
+        throw new IllegalStateException();
+    }
+
+    /**
+     * @param name the name of the person whose death cause to return
+     * @return the death cause of the person with the given name.
+     * If the cause of death is not specified, the method will return "heart attack".
+     * @throws IllegalArgumentException if the provider name is not written in this DeathNote
+     */
+    public String getDeathCause(String name){
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * @param name the name of the person whose death cause to return
+     * @return the death details of the person with the given name,
+     * or an empty string if no details have been provided.
+     * @throws IllegalArgumentException if the provider name is not written in this DeathNote.
+     */
+    public String getDeathDetails(String name){
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * @param name the name of the person
+     * @return true if the given name is written in this DeathNote, false otherwise
+     */
+    public boolean isNameWritten(String name){
+        return false;
+    }
+}
--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,17 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
+class TestDeathNote {   
+    final DeathNote dn;
+
+
+    @BeforeEach
+    public void setUp(){
+        dn = new DeathNote();
+    }
 
 }
