ff4ba6a5bb489b948e32ab7a1326d5118c1ee11a
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-17 09:56:16 +0100

    Implementation of getRule() tests

--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,17 +1,38 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 
 import it.unibo.deathnote.api.DeathNote;
 import it.unibo.deathnote.impl.DeathNoteImpl;
 
-class TestDeathNote {   
-    final DeathNote dn;
-
+class TestDeathNote {
+    private DeathNote dn;
+    private final int ZERO_RULE_NUMBER = 0;
 
     @BeforeEach
-    public void setUp(){
-        dn = new DeathNote();
+    public void setUp() {
+        dn = new DeathNoteImpl();
+    }
+
+    public void testGetRuleWithZero () {
+        try {
+            dn.getRule(ZERO_RULE_NUMBER);
+            Assertion.fail("Expected exception but none was thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "The rule number passed does not exist");
+        }
+    }
+
+    public void testGetRuleWithBigNumber(){
+        try {
+            dn.getRule(DeathNote.RULES.size() + 1);
+            Assertion.fail("Expected exception but none was thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "The rule number passed does not exist");
+        }
     }
 
 }
